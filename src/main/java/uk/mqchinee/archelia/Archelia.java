37f3e393d2600a7f689ab1f74412198e990696ce
isaweye <book.a.quill@gmail.com>
2023-07-25 21:54:05 +0300

    Remove metrics (bstats)

--- src/main/java/uk/mqchinee/archelia/Archelia.java
+++ src/main/java/uk/mqchinee/archelia/Archelia.java
@@ -6,9 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import uk.mqchinee.archelia.plugin.ArcheliaConfig;
 import uk.mqchinee.archelia.plugin.Command;
-import uk.mqchinee.archelia.plugin.Metrics;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +21,6 @@ public final class Archelia extends JavaPlugin {
      * A list of plugins that are currently using the Archelia plugin as a dependency or soft dependency.
      */
     public static List<Plugin> currentlyUsing = new ArrayList<>();
-
-    @Getter public static ArcheliaConfig archeliaConfig;
 
     @Getter
     private static Logger pluginLogger;
@@ -44,7 +40,6 @@ public final class Archelia extends JavaPlugin {
     private void setup() {
         saveDefaultConfig();
         pluginLogger = this.getLogger();
-        archeliaConfig = new ArcheliaConfig();
 
         new Command();
         if (vault()) {
@@ -57,11 +52,6 @@ public final class Archelia extends JavaPlugin {
             if (plugin.getDescription().getDepend().contains("Archelia") || plugin.getDescription().getSoftDepend().contains("Archelia")) {
                 currentlyUsing.add(plugin);
             }
-        }
-
-        if (archeliaConfig.metrics()) {
-            Metrics metrics = new Metrics(this,19238);
-            metrics.addCustomChart(new Metrics.SingleLineChart("ubc", () -> currentlyUsing.size()));
         }
     }
 
