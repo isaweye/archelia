bc84d26c993d45287cd91d6e8c81853ac45bf6b0
isaweye <book.a.quill@gmail.com>
2024-01-01 21:45:29 +0200

    Various improvements

--- src/main/java/uk/mqchinee/archelia/scoreboard/tag/TagManager.java
+++ src/main/java/uk/mqchinee/archelia/scoreboard/tag/TagManager.java
@@ -9,7 +9,7 @@ public class TagManager {
 
     private final Player player;
     private final TagData tagData;
-    private TagManager(Player player) {
+    public TagManager(Player player) {
         this.player = player;
         if (Archelia.getTagData().containsKey(player)) {
             tagData = Archelia.getTagData().get(player);
