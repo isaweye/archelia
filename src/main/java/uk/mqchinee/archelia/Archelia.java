c21c0a02f77e2c6fa638566c31b511c5f82bbb2a
isaweye <book.a.quill@gmail.com>
2024-01-22 18:43:37 +0200

    Various improvements

--- src/main/java/uk/mqchinee/archelia/Archelia.java
+++ src/main/java/uk/mqchinee/archelia/Archelia.java
@@ -1,16 +1,12 @@
 package uk.mqchinee.archelia;
 
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-import uk.mqchinee.archelia.impl.UpdateChecker;
-import uk.mqchinee.archelia.net.GHUpdateChecker;
 import uk.mqchinee.archelia.scoreboard.tag.TagData;
-import uk.mqchinee.archelia.utils.TextUtils;
 
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 /**
  * The main class for the Archelia.
@@ -20,31 +16,11 @@ public class Archelia {
     @Getter private final Plugin plugin;
 
     @Getter private static HashMap<Player, TagData> tagData;
+    @Getter private static final Logger logger = Logger.getLogger("Archelia");
 
     public Archelia(Plugin plugin) {
         this.plugin = plugin;
         tagData = new HashMap<>();
-        sendConsoleMessage("Checking for updates...");
-        getChecker().check();
+        logger.info("Registered "+plugin.getName()+".");
     }
-
-    public static void sendConsoleMessage(String message) {
-        Bukkit.getServer().getConsoleSender().sendMessage(message);
-    }
-
-    private UpdateChecker getChecker() {
-        GHUpdateChecker checker = new GHUpdateChecker("isaweye", "archelia", (JavaPlugin) plugin);
-        checker.setOnLatest(() -> sendConsoleMessage("No updates available!"));
-        checker.setOnFailure(() -> sendConsoleMessage("Unable to check for updates."));
-        checker.setOnSuccess((latest) -> {
-            sendConsoleMessage("An update has been found!");
-            TextUtils.console(TextUtils.colorize("[Archelia] &eCurrent version: &f"+ checker.getVersion()));
-            TextUtils.console(TextUtils.colorize("[Archelia] &aLatest version: &f"+ latest));
-            TextUtils.console(TextUtils.colorize("[Archelia] &fChangelog: "));
-            checker.getDescription().forEach(Archelia::sendConsoleMessage);
-            sendConsoleMessage("Download here: "+ checker.getLink());
-        });
-        return checker;
-    }
-
 }
